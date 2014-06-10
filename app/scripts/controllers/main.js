7d8913d7b486a7ff99d1c389d6ab9fcda5b29a21
Danny Grove <danny@drgrovellc.com>
2014-06-10 03:02:37 -0400

    Adds Basic Search functionality.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('omdbApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, omdb) {
+    $scope.search = {};
+    $scope.search.tomatoes = true
+
+    /**
+     * Basic Search
+     * @public
+     */
+    $scope.basicSearch = function(){
+      omdb.getByTitle($scope.search).then(function(response){
+        $scope.movie = response.data 
+      })
+    }
   });
