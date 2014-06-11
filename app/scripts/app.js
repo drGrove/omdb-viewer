95ee1ca7649830ebb35e20d9e44b9635163d5011
Danny Grove <danny@drgrovellc.com>
2014-06-11 03:53:49 -0400

    Adds movieSearch and movie paths

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,14 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/s/:movieTitle', {
+        templateUrl: 'views/movieSearch.html',
+        controller: 'MovieSearchCtrl'
+      })
+      .when('/i/:movieID', {
+        templateUrl: 'views/movie.html',
+        controller: 'MovieCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
