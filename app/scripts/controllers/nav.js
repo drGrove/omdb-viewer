bf78fdc9b0a881ba8087ff77c8ebe4702153f02e
Danny Grove <danny@drgrovellc.com>
2014-06-11 05:42:32 -0400

    Updates nav to add search functionality

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,5 +1,24 @@
 'use strict';
 
 angular.module('omdbApp')
-  .controller('NavCtrl', function ($scope) {
+.controller('NavCtrl', function ($scope, omdb, $location, Storage) {
+    $scope.search = {};
+
+    /**
+     * Basic Search
+     * @public
+     */
+    $scope.search = function(){
+      omdb.search($scope.search).then(function(response){
+        if(response.data.Reponse == "False"){
+          Storage.set('lastSearch', null)
+          alert('The search query "' + $scope.search.s + '" gave no results');
+        } else {
+
+          var searchQuery = $scope.search.s.split(' ').join("+");
+          Storage.set('lastSearch', response.data.Search)
+          $location.path('/s/' + searchQuery)
+        }
+      })
+    }
   });
